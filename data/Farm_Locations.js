f0b9ff19cdd12232010f98b4bef6f8fd4d1fce04
JOHN KENNEDY BOAFO <65027196+Jkboafo22@users.noreply.github.com>
2023-09-19 00:11:29 +0000

    Initial commit

---
+++ data/Farm_Locations.js
@@ -0,0 +1,15 @@
+var farms = {
+"type": "FeatureCollection",
+"name": "Farm_Locations",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Farms": "A", "Lat": 10.30237, "Lon": -1.51459, "Location": "WA East " }, "geometry": { "type": "Point", "coordinates": [ -1.51459, 10.30237 ] } },
+{ "type": "Feature", "properties": { "Farms": "B", "Lat": 10.3023, "Lon": -1.51468, "Location": "WA East " }, "geometry": { "type": "Point", "coordinates": [ -1.51468, 10.3023 ] } },
+{ "type": "Feature", "properties": { "Farms": "C", "Lat": 10.30221, "Lon": -1.51475, "Location": "WA East " }, "geometry": { "type": "Point", "coordinates": [ -1.51475, 10.30221 ] } },
+{ "type": "Feature", "properties": { "Farms": "D", "Lat": 10.30219, "Lon": -1.51486, "Location": "WA East " }, "geometry": { "type": "Point", "coordinates": [ -1.51486, 10.30219 ] } },
+{ "type": "Feature", "properties": { "Farms": "E", "Lat": 9.840338032, "Lon": -2.324628392, "Location": "WA East " }, "geometry": { "type": "Point", "coordinates": [ -2.324628392, 9.840338032 ] } },
+{ "type": "Feature", "properties": { "Farms": "F", "Lat": 5.943613, "Lon": 0.631313, "Location": "Agordome" }, "geometry": { "type": "Point", "coordinates": [ 0.631313, 5.943613 ] } },
+{ "type": "Feature", "properties": { "Farms": "G", "Lat": 5.943449, "Lon": 0.631268, "Location": "Agordome" }, "geometry": { "type": "Point", "coordinates": [ 0.631268, 5.943449 ] } },
+{ "type": "Feature", "properties": { "Farms": "H", "Lat": 7.32359, "Lon": -1.382032, "Location": "Aframso" }, "geometry": { "type": "Point", "coordinates": [ -1.382032, 7.32359 ] } }
+]
+}
---
+++ js/map.js
@@ -0,0 +1,135 @@
+//initialize map
+var map = L.map('map').setView([7.0, -1.09], 7);
+
+
+// add Osm title layer to map
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+ //Adding several Basemaps to leaflet
+var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+
+var googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+
+var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+})
+//.addTo(map);
+
+
+var googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+
+
+//Region Layer Style
+var regionStyle = {
+    color : "blue",
+    fillColor:"White",
+    opacity : 0.3,
+    weight: 1
+}
+
+//Greater Accra Layer Style
+var accraStyle = {
+    color : "red",
+    fillColor:"White",
+    opacity : 0.5,
+    weight: 2
+}
+
+//Farm Location Style
+var farmsStyle = {
+    radius:5,
+    fillColor:"black",
+    color:"red",
+}
+
+
+
+
+//Add geojson layers to map
+
+var regionlayer = L.geoJson(region,{
+    style:regionStyle,
+    onEachFeature:function (feature,layer){
+        layer.bindPopup(feature.properties.region)
+    }
+}).addTo(map)
+
+
+
+
+
+
+var farmslayer = L.geoJson(farms,{
+
+    pointToLayer:function(feature,latlng) {
+    return L.circleMarker(latlng, farmsStyle);   
+},
+onEachFeature:function (feature,layer){
+
+    label = `Farm Location: ${feature.properties.Location}<br>`
+    label += `Farms size: ${feature.properties.Null}<br>`
+
+    layer.bindPopup(label)
+    }
+}).addTo(map)
+
+
+
+
+
+//Basemaps
+var baseLayers = {
+    "OpenStreetMap": osm,
+    "Google Street Map": googleStreets,
+    "Google Hybrid": googleHybrid,
+    "Google Satelite": googleSat,
+    "Google Terrain": googleTerrain,
+};
+
+
+//Layers
+var overlays = {
+    "Ghana Regions": regionlayer,
+    "Farm Locations": farmslayer,
+
+};
+
+
+//Add layer control to map
+L.control.layers(baseLayers, overlays,{collapsed:false}).addTo(map);
+
+
+//Add leaflet browser print control to map
+
+L.control.browserPrint({position: 'topleft'}).addTo(map);
+
+
+//Add mouse move cordinates
+map.on("mousemove", function(e){
+
+	$("#coordinate").html(`Lat:${e.latlng.lat.toFixed(4)}, Lng:${e.latlng.lng.toFixed(4)}`)
+})
+
+
+//add Leaflet print controls
+ 
+//Add scale to map
+L.control.scale().addTo(map)
+
